3d373595a54ef3fef2f3079c4c85955b2b12a704
Elyees Tatua <elyeestatua@gmail.com>
2025-02-27 04:09:43 +0300

    fix : updated the hero section

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,12 @@ export default function Home() {
 
       {/* Hero */}
       <section>
-        <div>
+        <div className="container mx-auto">
           <div className="flex flex-col items-center gap-10 text-center mb-12">
             <AgentPulse size="large" color="blue" />
 
-            <h1 className="text-4xl md:text-6xl font-bold text-gray-900 mb-6">Meet your personal {""}</h1>
-          <span className="bg-gradient-to-r from-blue-600 to-blue-400 bg-clip-text text-transparent">AI Content Agent</span>
+            <h1 className="text-4xl md:text-6xl font-bold text-gray-900 mb-6">Meet your personal{" "}
+          <span className="bg-gradient-to-r from-blue-600 to-blue-400 bg-clip-text text-transparent"> AI Content Agent</span></h1>
           </div>
         </div>
       </section>
--- components/Header.tsx
+++ components/Header.tsx
@@ -47,7 +47,7 @@ const Header = () => {
                     </div>
                     </div >
                 </div>
-                Header</header>
+            </header>
         
   )
 }
