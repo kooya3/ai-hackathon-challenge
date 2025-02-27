1bdac5411d215609023bd0e0de4d42496f06ea6e
Elyees Tatua <elyeestatua@gmail.com>
2025-02-27 07:14:39 +0300

    style : updated the analyse button component

--- components/AnalyseButton.tsx
+++ components/AnalyseButton.tsx
@@ -10,7 +10,7 @@ function AnalyseButton() {
     <button
     type="submit"
     disabled={pending}
-    className="px-6 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2"
+    className="px-6 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed"
     >
 
     </button>
