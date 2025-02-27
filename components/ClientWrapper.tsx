de95f096a6083d5c0f3a2970b4b56e9c03e7dd13
Elyees Tatua <elyeestatua@gmail.com>
2025-02-27 15:14:05 +0300

    feat : added schematic wrapper functionality

--- components/ClientWrapper.tsx
+++ components/ClientWrapper.tsx
@@ -1,15 +1,26 @@
 "use client"
 
 import { ClerkProvider } from "@clerk/nextjs"
+import { Schematic, SchematicProvider } from "@schematichq/schematic-react"
+import SchematicWrapped from "./SchematicWrapped";
 
 export default function ClientWrapper({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+
+  const schematicPubKey = process.env.NEXT_PUBLIC_SCHEMATIC_PUBLISHABLE_KEY;
+  if (!schematicPubKey) {
+    throw new Error("Missing Schematic public key")
+  }
   return (
       <ClerkProvider>
+        <SchematicProvider publishableKey="schematicPubKey">
+          <SchematicWrapped>
           {children}
+          </SchematicWrapped>
+        </SchematicProvider>
       </ClerkProvider>
 
   )
